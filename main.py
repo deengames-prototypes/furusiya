07df19c18d83df158473f8445839aa49f602beb9
nightblade9 <nightbladecodes@gmail.com>
2017-11-03 17:23:01 -0400

    Convert tutorial to tdl.

--- main.py
+++ main.py
@@ -1,95 +1,60 @@
-#!/usr/bin/env python
-import tcod
+#!/usr/bin/env python3
  
-# ######################################################################
-# Global Game Settings
-# ######################################################################
-# Windows Controls
-FULLSCREEN = False
-SCREEN_WIDTH = 80  # characters wide
-SCREEN_HEIGHT = 50  # characters tall
-LIMIT_FPS = 20  # 20 frames-per-second maximum
-# Game Controls
-TURN_BASED = True  # turn-based game
+import tdl
  
-player_x = 0
-player_y = 0
+#actual size of the window
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
  
-def initialize_game():
-    global player_x, player_y
-    
-    # Setup player
-    player_x = SCREEN_WIDTH // 2
-    player_y = SCREEN_HEIGHT // 2
- 
-    # Setup Font
-    font_filename = 'arial10x10.png'
-    tcod.console_set_custom_font(font_filename, tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
- 
-    # Initialize screen
-    title = 'Python 3 + Libtcod tutorial'
-    tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, FULLSCREEN)
- 
-    # Set FPS
-    tcod.sys_set_fps(LIMIT_FPS)
- 
- 
-# ######################################################################
-# User Input
-# ######################################################################
-def get_key_event(turn_based=None):
-    if turn_based:
-        # Turn-based game play; wait for a key stroke
-        key = tcod.console_wait_for_keypress(True)
-    else:
-        # Real-time game play; don't wait for a player's key stroke
-        key = tcod.console_check_for_keypress()
-    return key
- 
+LIMIT_FPS = 20  #20 frames-per-second maximum
  
 def handle_keys():
-    global player_x, player_y
+    global playerx, playery
+  
+    #turn-based
+    user_input = tdl.event.key_wait()
  
-    key = get_key_event(TURN_BASED)
+    if user_input.key == 'ENTER' and user_input.alt:
+        #Alt+Enter: toggle fullscreen
+        tdl.set_fullscreen(not tdl.get_fullscreen())
  
-    if key.vk == tcod.KEY_ENTER and key.lalt:
-        # Alt+Enter: toggle fullscreen
-        tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
+    elif user_input.key == 'ESCAPE':
+        return True  #exit game
  
-    elif key.vk == tcod.KEY_ESCAPE:
-        return True  # exit game
+    #movement keys
+    if user_input.key == 'UP':
+        playery -= 1
  
-    # movement keys
-    if tcod.console_is_key_pressed(tcod.KEY_UP) or key.c == ord('w'):
-        player_y -= 1
+    elif user_input.key == 'DOWN':
+        playery += 1
  
-    elif tcod.console_is_key_pressed(tcod.KEY_DOWN) or key.c == ord('s'):
-        player_y += 1
+    elif user_input.key == 'LEFT':
+        playerx -= 1
  
-    elif tcod.console_is_key_pressed(tcod.KEY_LEFT) or key.c == ord('a'):
-        player_x -= 1
- 
-    elif tcod.console_is_key_pressed(tcod.KEY_RIGHT) or key.c == ord('d'):
-        player_x += 1
+    elif user_input.key == 'RIGHT':
+        playerx += 1
  
  
 #############################################
-# Main Game Loop
+# Initialization & Main Loop                #
 #############################################
  
+tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
+console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Roguelike", fullscreen=False)
+tdl.setFPS(LIMIT_FPS)
  
-def main():
-    initialize_game()
-    global player_x, player_y
+playerx = SCREEN_WIDTH//2
+playery = SCREEN_HEIGHT//2
  
-    exit_game = False
-    while not tcod.console_is_window_closed() and not exit_game:
-        tcod.console_set_default_foreground(0, tcod.white)
-        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
-        tcod.console_flush()
-        tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE)
+while not tdl.event.is_window_closed():
  
-        exit_game = handle_keys()
+    console.draw_char(playerx, playery, '@', bg=None, fg=(255,255,255))
  
+    tdl.flush()
  
-main()
+    console.draw_char(playerx, playery, ' ', bg=None)
+ 
+    #handle keys and exit game if needed
+    exit_game = handle_keys()
+    if exit_game:
+        break
