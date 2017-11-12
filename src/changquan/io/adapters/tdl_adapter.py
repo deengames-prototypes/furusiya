a91f6e4e2bd6d21f19db474bd49a5494cdc88d30
nightblade9 <nightbladecodes@gmail.com>
2017-11-11 23:12:13 -0500

    PoC forest generator and tdl_adapter method Architecture could/should be reworked.

--- src/changquan/io/adapters/tdl_adapter.py
+++ src/changquan/io/adapters/tdl_adapter.py
@@ -6,3 +6,23 @@ class TdlAdapter:
         tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
         self.console = tdl.init(width, height, title=window_title, fullscreen=False)
         tdl.setFPS(fps_limit)
+
+    # TODO: maybe we don't want this design ...
+    def draw_walls(self, data):
+        width = len(data)
+        height = len(data[0])
+
+        for y in range(0, height):
+            for x in range(0, width):
+                if data[x][y] == True:
+                    char = 'T'
+                    fg = (0, 128, 0)
+                else:
+                    char = '.'
+                    fg = (0, 64, 0)
+                self.console.draw_str(x, y, char, fg, bg=None)
+
+        # draw on-screen
+        tdl.flush()
+        # wait for response
+        key = tdl.event.key_wait()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from changquan.io.adapters.tdl_adapter import TdlAdapter
+from changquan.generators.forest_generator import ForestGenerator
 
 #actual size of the window
 SCREEN_WIDTH = 80
@@ -6,3 +7,16 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
 ui_adapter = TdlAdapter('Changquan Dad', SCREEN_WIDTH, SCREEN_HEIGHT)
+
+fg = ForestGenerator(SCREEN_WIDTH, SCREEN_HEIGHT, False)
+# Instead of doing this in one shot, do it in batches so we get copses of trees
+total = 1/6 * SCREEN_WIDTH * SCREEN_HEIGHT
+
+while total > 0:
+    batch = min(10, total)
+    total -= batch
+    fg.drunken_man_walk(batch, True)
+
+
+# DRAW IT!
+ui_adapter.draw_walls(fg.data)
