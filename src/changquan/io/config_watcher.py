e533052f9452dd2ac0d36c49735597e502251b0a
nightblade9 <nightbladecodes@gmail.com>
2017-11-15 12:58:24 -0500

    Config watcher watches, updates config Data is stored internally as a dictionary for now.

--- src/changquan/io/config_watcher.py
+++ src/changquan/io/config_watcher.py
@@ -4,21 +4,43 @@
 # We don't use "watchdog", because a) it requires a folder (or custom code)
 # to just watch a file, and b) it locks the file; see:
 # https://stackoverflow.com/questions/18599339/python-watchdog-monitoring-file-for-changes
-import time
+import json
+from json.decoder import JSONDecodeError
+import os
+import traceback
+
 from changquan.timer.repeating_timer import RepeatingTimer
 
 class ConfigWatcher:
 
-    CONFIG_FILE_NAME = 'config.json'
+    CONFIG_FILE_NAME = 'assets/data/config.json'
     instance = None
 
     def __init__(self):
         ConfigWatcher.instance = self
+
+        self.data = {}
+        self.last_changed_on = None
+        self.poll_file_for_changes()
+
         self.timer = RepeatingTimer(1, self.poll_file_for_changes)
-        self.timer.start()        
+        self.timer.start()  
 
     def poll_file_for_changes(self):
-        pass
+        modified_on = os.path.getmtime(ConfigWatcher.CONFIG_FILE_NAME)
+        if (modified_on != self.last_changed_on):
+            self.last_changed_on = modified_on
+            self.refresh_config()
+
+    def refresh_config(self):
+        with open(ConfigWatcher.CONFIG_FILE_NAME) as json_data:
+            try:
+                self.data = json.load(json_data)
+                print("Config.json updated: {0}".format(self.data))
+            except JSONDecodeError as ex:
+                print("Error updating config.json: {0}".format(ex))
+                #traceback.print_exc() # prints the stack-trace
+                # don't "raise", we want to keep retrying
 
     def dispose(self):
         self.timer.stop()
