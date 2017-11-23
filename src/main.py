00e1f65fd810a72740e64b1c56d8335f18511466
nightblade9 <nightbladecodes@gmail.com>
2017-11-23 16:39:26 -0500

    Wrap main module in function Still executable via cmdline

--- src/main.py
+++ src/main.py
@@ -6,35 +6,39 @@ import random
 import time
 import sys
 
-config = ConfigWatcher()
+def main():
+    config = ConfigWatcher()
 
-# Hard-coded random seed for easier debugging
-random.seed(config.get("universeSeed"))
+    # Hard-coded random seed for easier debugging
+    random.seed(config.get("universeSeed"))
 
-#actual size of the window
-SCREEN_WIDTH = 60
-SCREEN_HEIGHT = 40
-FPS_LIMIT = 20
+    #actual size of the window
+    SCREEN_WIDTH = 60
+    SCREEN_HEIGHT = 40
+    FPS_LIMIT = 20
 
-ui_adapter = TdlAdapter('Changquan Dad', SCREEN_WIDTH, SCREEN_HEIGHT, FPS_LIMIT)
+    ui_adapter = TdlAdapter('Changquan Dad', SCREEN_WIDTH, SCREEN_HEIGHT, FPS_LIMIT)
 
-end_game = False
+    end_game = False
 
-while not end_game:
-    map = Map(SCREEN_WIDTH, SCREEN_HEIGHT)
-    fg = ForestGenerator(SCREEN_WIDTH, SCREEN_HEIGHT)
-    fg.generate_trees(map)
+    while not end_game:
+        map = Map(SCREEN_WIDTH, SCREEN_HEIGHT)
+        fg = ForestGenerator(SCREEN_WIDTH, SCREEN_HEIGHT)
+        fg.generate_trees(map)
 
-    # DRAW IT!
-    for y in range(0, map.height):
-        for x in range(0, map.width):
-            tile = map.tiles[x][y]
-            ui_adapter.draw(x, y, tile.character, tile.colour)
+        # DRAW IT!
+        for y in range(0, map.height):
+            for x in range(0, map.width):
+                tile = map.tiles[x][y]
+                ui_adapter.draw(x, y, tile.character, tile.colour)
 
-    ui_adapter.flush()
+        ui_adapter.flush()
 
-    key = ui_adapter.wait_for_input()
-    if (key.key == "ESCAPE" or key.char.lower() == 'q'):
-        end_game = True
+        key = ui_adapter.wait_for_input()
+        if (key.key == "ESCAPE" or key.char.lower() == 'q'):
+            end_game = True
 
-config.dispose()
+    config.dispose()
+
+if __name__ == "__main__":
+    main()
