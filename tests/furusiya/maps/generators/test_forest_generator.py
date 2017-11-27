bccddd0c2a436d577bf37341b06d33ecb8e8f506
nightblade9 <nightbladecodes@gmail.com>
2017-11-27 17:31:28 -0500

    Test for tree-gap-filling.

--- tests/furusiya/maps/generators/test_forest_generator.py
+++ tests/furusiya/maps/generators/test_forest_generator.py
@@ -1,4 +1,5 @@
 import math
+import random
 import unittest
 
 from src.furusiya.maps.generators.forest_generator import ForestGenerator
@@ -22,3 +23,27 @@ class TestForestGenerator(unittest.TestCase):
 
         # might be more trees because of filled gaps between trees
         self.assertGreaterEqual(actual_num_trees, expected_num_trees)
+
+    def test_generate_trees_fills_holes(self):
+        
+        # Generate a bunch of trees with a known seed that generates holes.
+        # This is fragile, but there's no other way to test this.
+        # This is valuable, because there's a ton of code/complexity behind
+        # this (breadth-first search, etc.).
+        width, height = (20, 10)
+        map = Map(width, height)        
+        fg = ForestGenerator(width, height)
+        pre_fill_num_trees = math.floor(ForestGenerator.TREE_PERCENTAGE * width * height)
+
+        random.seed(1)
+        fg.generate_trees(map)
+
+        actual_num_trees = 0
+
+        for y in range(height):
+            for x in range(width):
+                if map.tiles[x][y].is_walkable == False:
+                    actual_num_trees += 1
+
+        # Strictly more trees because of filled holes
+        self.assertGreater(actual_num_trees, pre_fill_num_trees)
