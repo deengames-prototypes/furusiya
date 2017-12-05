22a8288154751f96fe9012f514c8d44d3a771f96
nightblade9 <nightbladecodes@gmail.com>
2017-12-05 14:22:36 -0500

    recalculate_fov test. Draw test is impossible, Mock doesn't allow replacing a method with a callback.

--- tests/ix/test_map_renderer.py
+++ tests/ix/test_map_renderer.py
@@ -6,6 +6,7 @@ from furusiya.io.map_renderer import MapRenderer
 from furusiya.io.adapters.tdl_adapter import TdlAdapter
 from furusiya.maps.map import Map
 from unittest.mock import MagicMock
+from unittest.mock import patch
 
 class TestMapRenderer(unittest.TestCase):
 
@@ -45,7 +46,35 @@ class TestMapRenderer(unittest.TestCase):
             self.assertTrue(map.tiles[x][y].is_explored)
 
     def test_render_recalculates_fov_when_asked(self):
-        pass
+        map_width = 10
+        map_height = 10
+        map = Map(map_width, map_height)
 
-    def test_render_draws_tiles_in_fov(self):
-        pass
+        # Player is at (0, 0)
+        player = Player()
+
+        # Mock the FOV tiles.
+        light_radius = 5
+        fov_tiles = []        
+
+        for i in range(light_radius):
+            fov_tiles.append((i, 0)) # horizontal ray
+            fov_tiles.append((0, i)) # vertical ray
+
+        tdl_adapter = TdlAdapter("Test Window", map_width, map_height)
+        tdl_adapter.calculate_fov = MagicMock(return_value = fov_tiles)
+
+        renderer = MapRenderer(map, player, tdl_adapter)
+        self.assertTrue(renderer.recompute_fov)
+        renderer.render() # calls calculate_fov
+        
+        self.assertFalse(renderer.recompute_fov)        
+        tdl_adapter.calculate_fov.reset_mock() # reset call count to 0
+        renderer.render() # doesn't call calculate_fov
+        tdl_adapter.calculate_fov.assert_not_called()
+
+        renderer.recompute_fov = True
+        renderer.render()
+
+        tdl_adapter.calculate_fov.assert_called_with(player.x, player.y, map.is_walkable,
+            MapRenderer.FOV_ALGORITHM, light_radius, MapRenderer.SHOULD_LIGHT_WALLS)
