bdb74924b30379d2e2a1183390c3263aa97c106b
nightblade9 <nightbladecodes@gmail.com>
2017-12-05 12:45:33 -0500

    Stop using deprecated function.

--- furusiya/io/adapters/tdl_adapter.py
+++ furusiya/io/adapters/tdl_adapter.py
@@ -5,8 +5,7 @@ class TdlAdapter:
     def __init__(self, window_title, width, height, fps_limit=20):
         tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
         self.console = tdl.init(width, height, title=window_title, fullscreen=False)
-        tdl.setFPS(fps_limit)
-
+        tdl.set_fps(fps_limit)
 
     # TODO: maybe we don't want this design ...
     def draw(self, x, y, char, colour):
