8c02a2c6b358e39232988677f07114c810bafd7a
NegativeScript <ala2aldeen913@gmail.com>
2018-02-07 20:42:11 +0300

    Fixed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,11 @@ import sys
 
 import pytest
 
-import config
 
 path_ = os.path.join(os.getcwd(), 'furusiya')
 sys.path.append(path_)
 
+import config
 
 with open(os.path.join(path_, 'config.json')) as f:
     config.load(f.read())
