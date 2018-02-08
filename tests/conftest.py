588467d6fc10b07b87cac8231210e17c99eba2eb
NegativeScript <ala2aldeen913@gmail.com>
2018-02-08 20:10:44 +0300

    Made requested mini-changes and fixed tests, for real!

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,19 @@
 import os
 import sys
 
+import pytest
+
 path_ = os.path.join(os.getcwd(), 'furusiya')
 sys.path.append(path_)
 
-from model.config import config
+import model.config.config as prod_config
 
-with open(os.path.join(path_, 'config.json')) as f:
-    config.load(f.read())
+with open(os.path.join(path_, 'config.json'), 'rt') as f:
+    raw_json = f.read()
+
+prod_config.load(raw_json)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def config(request):
+    return prod_config
