3916a4533a419db79186379568da154936dfbd83
NegativeScript <ala2aldeen913@gmail.com>
2018-02-10 16:53:02 +0300

    Stallion now follows the player around

--- furusiya/model/components/ai/stallion.py
+++ furusiya/model/components/ai/stallion.py
@@ -3,4 +3,5 @@ from model.components.ai.base import AbstractAI
 
 class StallionAi(AbstractAI):
     def _take_turn(self):
-        print("Stallion whinnies!")
+        if self.owner.distance_to(self.owner.player) >= 2:
+            self.owner.move_towards(self.owner.player.x, self.owner.player.y)
---
+++ tests/furusiya/model/components/ai/test_stallion.py
@@ -0,0 +1,27 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from model.components.ai.stallion import StallionAi
+
+
+@pytest.fixture
+def stallion():
+    stallion_mock = MagicMock(x=2, y=0)
+    stallion_mock.player = MagicMock(x=4, y=1)
+    stallion_mock.ai = StallionAi(stallion_mock)
+    yield stallion_mock
+
+
+def test_take_turn_far(stallion):
+    stallion.distance_to.return_value = 5
+    stallion.ai.take_turn()
+
+    stallion.move_towards.assert_called_with(stallion.player.x, stallion.player.y)
+
+
+def test_take_turn_close(stallion):
+    stallion.distance_to.return_value = 1
+    stallion.ai.take_turn()
+
+    assert stallion.move_towards.call_count == 0
