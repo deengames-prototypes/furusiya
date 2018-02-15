5b6917863a600bd7baefce98ecb304b1bc581af3
NegativeScript <ala2aldeen913@gmail.com>
2018-02-15 22:15:57 +0300

    Changed OmniSlash's `process` method to a static method

--- furusiya/model/skills/omnislash.py
+++ furusiya/model/skills/omnislash.py
@@ -2,8 +2,8 @@ import random
 
 
 class OmniSlash:
-    @classmethod
-    def process(cls, player, rehit_percent, delta_coords):
+    @staticmethod
+    def process(player, rehit_percent, delta_coords):
         should_re_hit = random.randint(0, 100) <= rehit_percent
         if should_re_hit:
             player.move_or_attack(*delta_coords)
