fda98fe9d7be6a0934cd0927061c11452e7361f2
NegativeScript <ala2aldeen913@gmail.com>
2018-02-16 12:21:13 +0300

    Cleaned up whirlwind test as requested

--- tests/furusiya/model/skills/test_whirlwind.py
+++ tests/furusiya/model/skills/test_whirlwind.py
@@ -7,49 +7,31 @@ from model.skills.whirlwind import Whirlwind
 from model.systems.fighter_system import FighterSystem
 
 
-def _mock_factory(*args, **kwargs):
+def _mock_factory(*args, fighter=None, **kwargs):
+    fighter = fighter or Mock()
     m = Mock(*args, **kwargs)
+    FighterSystem.set_fighter(m, fighter)
     m.distance.side_effect = lambda x, y: math.sqrt((x - m.x) ** 2 + (y - m.y) ** 2)
     return m
 
 
 @pytest.fixture
-def player():
-    yield _mock_factory(x=1, y=2)
-
-
-@pytest.fixture
-def player_fighter():
-    yield Mock()
-
-
-@pytest.fixture
-def bushslime():
-    yield _mock_factory(x=7, y=8)
-
-
-@pytest.fixture
-def _fighter():
-    yield Mock()
-
-
-@pytest.fixture
-def tigerslash():
-    yield _mock_factory(x=2, y=2)
-
-
-@pytest.fixture
-def whirlwind(player, player_fighter, bushslime, tigerslash, _fighter):
-    FighterSystem.set_fighter(player, player_fighter)
-    FighterSystem.set_fighter(bushslime, _fighter)
-    FighterSystem.set_fighter(tigerslash, _fighter)
+def whirlwind():
     yield Whirlwind
 
 
-def test_process(whirlwind, player, player_fighter, tigerslash, bushslime):
-    whirlwind.process(player, 2, Mock(entities=[player, bushslime, tigerslash]))
+def test_process(whirlwind):
+    player_fighter = Mock()
+    player = _mock_factory(x=1, y=2, fighter=player_fighter)
 
-    player_fighter.attack.assert_called_with(tigerslash)
+    bushslime = _mock_factory(x=7, y=8)
+    tigerslash = _mock_factory(x=2, y=2)
+    steelhawk = _mock_factory(x=2, y=1)
+
+    whirlwind.process(player, 2, Mock(entities=[player, bushslime, tigerslash, steelhawk]))
+
+    player_fighter.attack.assert_any_call(tigerslash)
+    player_fighter.attack.assert_any_call(steelhawk)
 
     with pytest.raises(AssertionError):
         player_fighter.attack.assert_called_with(player)
