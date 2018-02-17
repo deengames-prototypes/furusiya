ddcb63fa1263a245e8c67387745f7f149e4371f3
NegativeScript <ala2aldeen913@gmail.com>
2018-02-17 10:18:11 +0300

    Ridiculously slow test for new_game

---
+++ tests/furusiya/test_main.py
@@ -0,0 +1,44 @@
+from game import Game
+from main import new_game
+from model.systems.xp_system import XPSystem
+
+
+class TestNewGame:
+    def test_basic(self):
+        new_game()
+
+        assert Game.area_map is not None
+        assert (Game.area_map.width, Game.area_map.height) != (0, 0)
+        assert Game.area_map.tiles != []  # map has been generated
+
+        assert Game.player is not None
+        assert Game.stallion is not None
+
+        assert (Game.stallion.x, Game.stallion.y) in (
+            (x + Game.player.x, y + Game.player.y)
+            for x, y in (
+                (dx, dy)
+                for dx in range(-1, 2)
+                for dy in range(-1, 2)
+            )
+        )
+
+        assert XPSystem.get_experience(Game.player).level == 5
+
+        assert Game.inventory is not None
+        assert Game.inventory == []
+
+        assert Game.game_state is not None
+        assert Game.game_state == 'playing'
+
+        assert Game.game_msgs is not None
+        assert len(Game.game_msgs) == 5  # 1 welcoming message, 4 level up messages.
+
+        old_tile = (Game.player.x, Game.player.y)
+        assert Game.area_map.tiles[Game.player.x][Game.player.y].is_walkable
+
+        new_game()
+        new_tile = (Game.player.x, Game.player.y)
+        assert Game.area_map.tiles[Game.player.x][Game.player.y].is_walkable
+
+        assert old_tile != new_tile
