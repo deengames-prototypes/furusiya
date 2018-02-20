366c5cd9ada8f89585a5d256f35129a4c8b3534e
NegativeScript <ala2aldeen913@gmail.com>
2018-02-20 22:59:22 +0300

    Removed seed hack

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ path_ = os.path.join(os.getcwd(), 'furusiya')
 sys.path.append(path_)
 
 import model.config.config as prod_config
-from game import Game
 from main import init_game
 
 with open(os.path.join(path_, 'config.json'), 'rt') as f:
@@ -25,8 +24,3 @@ def config(request):
 @pytest.fixture(scope='session', autouse=True)
 def initialize_game_for_tests():
     init_game()
-
-
-@pytest.fixture(scope='session', autouse=True)
-def seed():
-    Game.random.seed(1)
