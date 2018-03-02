2efe61a6f5312a66a9766dd037464315e1b1c5c2
NegativeScript <ala2aldeen913@gmail.com>
2018-03-02 17:49:54 +0300

    Renamed conf -> config

--- furusiya/model/skills/frostbomb.py
+++ furusiya/model/skills/frostbomb.py
@@ -3,8 +3,8 @@ from model.components.ai.monster import FrozenMonster
 
 class FrostBomb:
     @staticmethod
-    def process(area_map, player, ai_system, conf):
-        center = conf.radius // 2
+    def process(area_map, player, ai_system, config):
+        center = config.radius // 2
         for x in range(player.x - center, player.x + center + 1):
             for y in range(player.y - center, player.y + center + 1):
                 entities = (
@@ -13,4 +13,4 @@ class FrostBomb:
                     if e.hostile
                 )
                 for entity in entities:
-                    ai_system.get(entity).temporarily_switch_to(FrozenMonster(entity, conf.turnsToThaw))
+                    ai_system.get(entity).temporarily_switch_to(FrozenMonster(entity, config.turnsToThaw))
