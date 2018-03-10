7ea4a9c09e64a71cdb864211e40f795b25ad352b
NegativeScript <ala2aldeen913@gmail.com>
2018-03-10 23:17:32 +0300

    Better split fixtures

--- tests/furusiya/model/weapons/test_spear.py
+++ tests/furusiya/model/weapons/test_spear.py
@@ -7,6 +7,13 @@ from model.weapons import Spear
 
 class TestSpear:
     fighter_dict = {}
+    entities = []
+
+    def _get_target(self, x, y, fighter=None):
+        t = Mock(x=x, y=y)
+        self.fighter_dict[t] = fighter or Mock(hostile=True)
+        self.entities.append(t)
+        return t
 
     @pytest.fixture
     def fighter(self):
@@ -14,35 +21,32 @@ class TestSpear:
 
     @pytest.fixture
     def immediate_target(self):
-        yield Mock(x=6, y=5)
+        yield self._get_target(x=6, y=5)
 
     @pytest.fixture
     def pierced_target(self):
-        yield Mock(x=7, y=5)
+        yield self._get_target(x=7, y=5)
 
     @pytest.fixture
-    def spear(self):
-        yield Spear(Mock(x=5, y=5))
+    def not_pierced_target(self):
+        yield self._get_target(x=8, y=5)
 
     @pytest.fixture
-    def game(self, spear, fighter, immediate_target, pierced_target):
+    def spear(self, fighter):
+        owner = self._get_target(x=5, y=5, fighter=fighter)
+        yield Spear(owner)
+
+    @pytest.fixture
+    def game(self):
         game = Mock()
-
-        self.fighter_dict.update({
-            spear.owner: fighter,
-            immediate_target: Mock(hostile=True),
-            pierced_target: Mock(hostile=True)
-        })
 
         game.fighter_system.get.side_effect = lambda e: self.fighter_dict.get(e, Mock())
         game.fighter_system.has.return_value = True
 
-        entities = [spear.owner, immediate_target, pierced_target]
-
         def get_blocking_object_at(x, y):
             objects = [
                 e
-                for e in entities
+                for e in self.entities
                 if (e.x, e.y) == (x, y)
             ]
             if len(objects) == 0:
@@ -75,15 +79,17 @@ class TestSpear:
 
         assert not fighter.attack.called
 
-    def test_attack_ignores_non_hostiles(self, spear, game, fighter, immediate_target, pierced_target):
-        self.fighter_dict[immediate_target].hostile = False
-        self.fighter_dict[pierced_target].hostile = False
+    def test_attack_ignores_non_hostiles(self, spear, game, fighter, immediate_target):
+        for v in self.fighter_dict.values():
+            v.hostile = False
 
         spear.attack(immediate_target, game)
 
         assert not fighter.attack.called
 
-    def test_attack_pierces_enemies(self, spear, game, fighter, immediate_target, pierced_target):
+    def test_attack_pierces_enemies(self, spear, game, fighter, immediate_target, pierced_target, not_pierced_target):
         spear.attack(immediate_target, game)
 
         fighter.attack.assert_any_call(pierced_target, recurse=False)
+        with pytest.raises(AssertionError):
+            fighter.attack.assert_any_call(not_pierced_target, recurse=False)
