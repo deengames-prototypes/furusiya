9fa6cefd7bcc93b8a85cfab5b04e435ab97d3ac7
NegativeScript <ala2aldeen913@gmail.com>
2018-03-13 19:47:29 +0300

    Tests for event bus

--- furusiya/model/event/event_bus.py
+++ furusiya/model/event/event_bus.py
@@ -2,20 +2,19 @@ class EventBus:
     def __init__(self):
         self.events = {}
 
-    def _ensure_exists(self, event_name):
+    def _create_data_structure(self, event_name):
         if not isinstance(self.events.get(event_name, None), list):
             self.events[event_name] = []
 
     def trigger(self, event_name, *args, **kwargs):
-        self._ensure_exists(event_name)
-
-        for callback in self.events[event_name]:
-            callback(*args, **kwargs)
+        if self.events.get(event_name, None):
+            for callback in self.events[event_name]:
+                callback(*args, **kwargs)
 
     def bind(self, event_name, event_callback):
-        self._ensure_exists(event_name)
+        self._create_data_structure(event_name)
         self.events[event_name].append(event_callback)
 
     def unbind(self, event_name, event_callback):
-        self._ensure_exists(event_name)
-        self.events[event_name].remove(event_callback)
+        if self.events.get(event_name, None):
+            self.events[event_name].remove(event_callback)
