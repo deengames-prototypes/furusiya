f8c61f52e35d3a308838f295b6f60fb5f1d38fad
nightblade9 <nightbladecodes@gmail.com>
2018-03-15 17:18:46 -0400

    Code-review fix for #79 Move turn-pass event into UpdateManager This breaks a test, but I have a fix coming.

--- furusiya/model/keys/update_manager.py
+++ furusiya/model/keys/update_manager.py
@@ -1,5 +1,5 @@
 from model.config import config
-
+from game import Game
 
 class UpdateManager:
     def __init__(self, game):
@@ -12,6 +12,7 @@ class UpdateManager:
         else:  # it's everyone else's turn
             self.take_enemy_turns()
             self.restore_skill_points()
+            self.game.events.trigger('on_turn_pass')            
 
     def take_enemy_turns(self):
         for e in self.game.area_map.entities:
