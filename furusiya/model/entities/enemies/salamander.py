0975ff4ef47a44c95e0278147238c2b898880fdb
NegativeScript <ala2aldeen913@gmail.com>
2018-03-16 11:23:38 +0300

    Salamander now spawns fire!

--- furusiya/model/entities/enemies/salamander.py
+++ furusiya/model/entities/enemies/salamander.py
@@ -1,5 +1,19 @@
+from game import Game
+from model.entities.fire import Fire
 from model.entities.game_object import GameObject
+from model.config import config
 
 
 class Salamander(GameObject):
-    pass
+    def __init__(self, x, y, char, name, color, blocks=False):
+        super().__init__(x, y, char, name, color, blocks=blocks)
+
+        Game.instance.event_bus.bind('on_turn_pass', self.on_turn_passed, self)
+
+    def on_turn_passed(self):
+        if config.data.enemies.salamander.shootsFire:
+            self.spawn_fire()
+
+    def spawn_fire(self):
+        x, y = Game.instance.area_map.get_walkable_tile_around(self.x, self.y, config.data.enemies.salamander.fireRange)
+        Game.instance.area_map.entities.append(Fire(x, y))
